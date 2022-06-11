03fffe9b89d9aa54853a7378c033f26208ef3c74
Alex <asenges400@gmail.com>
2022-06-11 15:42:07 -0300

    0x1B. C - Sorting algorithms & Big O

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,51 @@
 #include "sort.h"
 /**
+ * swap - helper function
+ * Return: void
+ * @a: int ptr
+ * @b: int ptr
+ * Description: Swap two elements in an array
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * partition - helper function
+ * Return: long int
+ * @array: int array
+ * @low: long int
+ * @high: long int
+ * @size: size_t
+ * Description: Retrieves a new portion of an array
+ */
+long int partition(int *array, long int low, long int high, size_t size)
+{
+	int pivot = array[high];
+	int i = (low - 1), j = 0;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[high] < array[i + 1])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+/**
  * quick_sort - caller to quick sorting function
  * Return: void
  * @array: int ptr
