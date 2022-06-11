7e107f4ecd1e42f72e836dd32b63615d6cc70d6e
root <mcarrascopiaggio@gmail.com>
2022-06-11 11:20:10 -0700

    first version 2 & fix betty 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,24 +15,16 @@ void insertion_sort_list(listint_t **list)
 	node = (*list)->next;
 	while (node)
 	{
-		/*Ubico el nodo next_node en el segundo*/
 		aux_node = node->next;
-		/*como voy a comparar con el anterior tengo que incluir la
-		 * la condicion de prev sea distinto de nulo
-		 * ademas de que el anterior sea mayoy al actual*/
 		while (node->prev != NULL && node->prev->n > node->n)
 		{
-			/*Asigno el nodo previo para la comparacion*/
 			tmp_node = node->prev;
-			/*Asigno el nodo siguiente*/
 			tmp_node->next = node->next;
 			if (node->next != NULL)
 				node->next->prev = node->prev;
-			/*swap de nodos*/
 			node->next = tmp_node;
 			node->prev = tmp_node->prev;
 			tmp_node->prev = node;
-			/* para el anterior ver caso especial primer nodo*/
 			if (node->prev == NULL)
 				*list = node;
 			else
